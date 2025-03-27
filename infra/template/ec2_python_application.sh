eec2155de34ca5c975228b1971f3be03fb564f11
DataOpsInnovator <mefinealew@gmail.com>
2025-03-26 22:54:40 -0400

    Updated devops project

--- infra/template/ec2_python_application.sh
+++ infra/template/ec2_python_application.sh
@@ -6,7 +6,7 @@ yes | sudo apt install python3 python3-pip
 https://github.com/DataOpsInnovator/python-mysql-db-project.git
 sleep 20
 # shellcheck disable=SC2164
-cd python-mysql-db-proj
+cd python-mysql-db-project
 pip3 install -r requirements.txt
 echo 'Waiting for 30 seconds before running the app.py'
 setsid python3 -u app.py &
