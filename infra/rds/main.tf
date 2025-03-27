5a55ff875f52b73cde59c1f1f0cd55f2ada644a8
Mesfine_Getachew <mesfin.g.tech@gmail.com>
2025-03-27 12:22:48 -0400

    Update main.tf

--- infra/rds/main.tf
+++ infra/rds/main.tf
@@ -16,8 +16,8 @@ resource "aws_db_instance" "default" {
   allocated_storage       = 10
   storage_type            = "gp2"
   engine                  = "mysql"
-  engine_version          = "5.7"
-  instance_class          = "db.t2.micro"
+  engine_version          = "5.7.34"  # Use a specific version like 5.7.34
+  instance_class          = "db.t3.micro"  # Switch to db.t3.micro
   identifier              = var.mysql_db_identifier
   username                = var.mysql_username
   password                = var.mysql_password
@@ -29,4 +29,5 @@ resource "aws_db_instance" "default" {
   backup_retention_period = 0
   deletion_protection     = false
 }
+
 
